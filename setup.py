6735a090e2bdbda70ffcabd75f095b27c3a3b81b
jrderuiter <julianderuiter@gmail.com>
2015-09-02 12:20:55 +0200

    Added url, description and classifiers to setup.py.

--- setup.py
+++ setup.py
@@ -5,15 +5,21 @@ install_requires = ['future', 'pandas', 'requests', 'requests_cache']
 setup(
     name='pybiomart',
     version='0.0.1',
-    url='',
+    url='https://github.com/jrderuiter/pybiomart',
     author='Julian de Ruiter',
     author_email='julianderuiter@gmail.com',
-    description='',
+    description='A simple pythonic interface to biomart. ',
     license='MIT',
     packages=find_packages(),
-    include_package_data=True,
-    extras_require={},
     zip_safe=True,
-    classifiers=[],
-    install_requires=install_requires
+    classifiers=['Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5'],
+    install_requires=install_requires,
+    extras_require={}
 )
