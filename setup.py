9a2316d292a9aed5764de85d6de1aa9391dd233a
jrderuiter <julianderuiter@gmail.com>
2015-09-06 11:15:59 +0200

    Use pyandoc to read description from README.md.

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    description = open('README.md').read()
+
 install_requires = ['future', 'pandas', 'requests', 'requests_cache']
 
 setup(
@@ -9,7 +15,7 @@ setup(
     download_url='https://github.com/jrderuiter/pybiomart/tarball/0.0.3',
     author='Julian de Ruiter',
     author_email='julianderuiter@gmail.com',
-    description='A simple pythonic interface to biomart. ',
+    description=description,
     license='MIT',
     packages=find_packages(),
     zip_safe=True,
